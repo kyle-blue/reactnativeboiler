7b63023f09547e01bb6f35cf81a3f2f629a9b2ac
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2022-07-01 22:00:49 +0100

    garbage mate

---
+++ ios/Pods/Target Support Files/Pods-reactnativetest-reactnativetestTests/Pods-reactnativetest-reactnativetestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactnativetest_reactnativetestTests : NSObject
+@end
+@implementation PodsDummy_Pods_reactnativetest_reactnativetestTests
+@end
